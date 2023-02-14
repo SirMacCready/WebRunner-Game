87fd7db363ce96c1da9e2215fecb64f2b6826376
Yohann DC <YohannDCz@gmail.com>
2023-02-14 11:10:47 +0100

    Bug fixes on add images

--- scripts/addObstacles.js
+++ scripts/addObstacles.js
@@ -1,10 +1,5 @@
 function addObstacle(type, newDiv) {
   if (type == "B") {
-    console.log("ok")
-  // const downObstacle = document.querySelectorAll(".B");
-  // const upObstacle = document.querySelectorAll(".C");
-  // const box2 = downObstacle.querySelector(".box2");
-  // const box1 = downObstacle.querySelector(".box1");
     let box = newDiv.querySelector(".box2");
     let images = ["blue", "red", "black"];
     let random = Math.floor(Math.random() * 3);
@@ -13,10 +8,7 @@ function addObstacle(type, newDiv) {
     image.classList = "image obstacle down-obstacle";
     box.appendChild(image);
   } else if (type == "C") {
-
-  // upObstacle.forEach((obstacle) => {
     let box = newDiv.querySelector(".box1");
-    // box.querySelectorAll("img").visibility = "hidden";
 
     for (let i = 1; i <= 7; i++) {
       let image = document.createElement("img");
