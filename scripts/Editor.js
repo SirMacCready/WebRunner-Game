caf56db97d1ef6a89973cafbb8dce124ff53ffb4
Yohann DC <YohannDCz@gmail.com>
2023-02-16 07:51:49 +0100

    Editeur bugué

--- scripts/Editor.js
+++ scripts/Editor.js
@@ -12,23 +12,7 @@ const delMenu = document.querySelector('.delete');
 addMenu.style.display = 'none';
 editMenu.style.display = 'none';
 
-
-// Fonction ajout de blocks
-
-function addBlockJSON(type, data) {
-    const containers = document.querySelectorAll(".container");
-    const menuItems = document.querySelector('#add' + type);
-
-    menuItems.addEventListener('click', () => {
-        let blockNum = (data.blocks).length
-        newObject = { "type": type }
-        let newDiv = document.createElement('div');
-        newDiv.className = 'container' + type;
-        newDiv.id = blockNum
-        data["blocks"].push(newObject)
-        document.getElementById('blocks').appendChild(newDiv)
-    })
-}
+// Fonction d'ajout de block
 
 function addBlock(type, data) {
     const containers = document.querySelectorAll(".container");
@@ -36,20 +20,22 @@ function addBlock(type, data) {
 
     containers.forEach((container) => {
         container.addEventListener('click', function addBlock1(e) {
-            menuItems.addEventListener('click', function addBlock2() {
-                let blockNum = (data.blocks).length
-                newObject = { "type": type }
+            menuItems.addEventListener('click', function () {
                 let newDiv = document.createElement('div');
                 newDiv.classList = 'container ' + type;
-                newDiv.id = blockNum
+                newDiv.id = parseInt(e.target.id);
                 let newBox = document.createElement('div');
                 newBox.className = "box1";
                 newDiv.appendChild(newBox);
-                newBox = document.createElement('div');
-                newBox.className = "box2";
-                newDiv.appendChild(newBox);
-                e.target.after(newDiv);
-                // data.blocks.splice(blocks.indexOf(e.target) + 1, 0, newObject)
+                let newBox2 = document.createElement('div');
+                newBox2.className = "box2";
+                newDiv.appendChild(newBox2);
+                e.target.parentNode.insertBefore(newDiv, e.target.nextSibling);
+                newObject = { "type": type }
+                data.blocks.splice(parseInt(e.target.id) + 1, 0, newObject)
+                for (let i = parseInt(e.target.id); i < containers.length; i++) {
+                    containers[i].id = parseInt(containers[i].id) + 1;
+                }
                 if (type == "A" || type == "B" || type == "C") {
                     addObstacle(type, newDiv);
                 }
@@ -57,44 +43,6 @@ function addBlock(type, data) {
             container.removeEventListener('click', addBlock1)
         })
     });
-}
-
-// function addBlock(a, data, addMenu) {
-//     addMenu.onclick = (bt) => {
-//         let btn = bt.target
-//         let newObject = { "type": btn.id.substr(btn.id.length - 1) }
-//         let newDiv = document.createElement('div');
-//         newDiv.classList = 'container ' + btn.id.substr(btn.id.length - 1)
-//         data["blocks"].push(newObject)
-//         let newBox = document.createElement('div');
-//         newBox.className = "box1";
-//         newDiv.appendChild(newBox);
-//         newBox = document.createElement('div');
-//         newBox.className = "box2";
-//         newDiv.appendChild(newBox);
-//         a.target.after(newDiv);
-//         data.blocks.splice(a.target.id, 1, newObject)
-//         if (btn.id.substr(btn.id.length - 1) == "A"
-//             || btn.id.substr(btn.id.length - 1) == "B"
-//             || btn.id.substr(btn.id.length - 1) == "C") {
-//             addObstacle(btn.id.substr(btn.id.length - 1), a.target);
-//         }
-//     }
-// }
-
-// Fonction de délétion de block
-
-function delBlock(a, data, delMenu) {
-    let oldA = null
-    delMenu.onclick = (bt) => {
-        let btn = bt.target
-        if (oldA != a) {
-            a.target.remove()
-            data.blocks.splice(a.target.id, 1)
-            oldA = a
-            return
-        }
-    }
 }
 
 // Fonction d'édit de block
@@ -115,6 +63,23 @@ function editBlock(a, data, editMenu) {
     }
 }
 
+// Fonction de délétion de block
+
+function delBlock(a, data, delMenu) {
+    let oldA = null
+    delMenu.onclick = (bt) => {
+        let btn = bt.target
+        if (oldA != a) {
+            a.target.remove()
+            data.blocks.splice(a.target.id, 1)
+            oldA = a
+            return
+        }
+    }
+}
+
+
+
 // Fonction d'affichage des menus
 
 function showHide() {
@@ -125,16 +90,16 @@ function showHide() {
     const hiddenEditMenu = document.querySelector(".editMenu");
 
     add.addEventListener('mouseover', () => {
-        addMenu.style.display = "flex";
+        addMenu.style.display = "block";
         hiddenAddMenu.onmouseover = function () {
-            addMenu.style.display = "flex";
+            addMenu.style.display = "block";
         }
     })
 
     edit.addEventListener('mouseover', () => {
-        editMenu.style.display = "flex";
+        editMenu.style.display = "block";
         hiddenEditMenu.onmouseover = function () {
-            editMenu.style.display = "flex";
+            editMenu.style.display = "block";
         }
     })
 
@@ -166,14 +131,8 @@ function save(content, fileName) {
     let a = document.createElement("a");
     let file = new Blob([content]);
     a.href = URL.createObjectURL(file);
-    a.download = fileName;
+    a.download = `${fileName}.jmpr`;
     a.click();
-}
-
-function saveDataJSON(elements, data, i) {
-    let String = elements.childNodes[i].className
-    let JSONObject = { type: String.substr(String.length - 1) }
-    data.blocks.push(JSONObject)
 }
 
 function loadEditJSON(levelURI) {
@@ -193,14 +152,9 @@ function loadEditJSON(levelURI) {
             showHide();
 
             document.addEventListener('click', function (e) {
-                if (hasClass(e.target, 'container')) {
-                    addBlock(e, data, addMenu);
+                if (hasClass(e.target, 'container')) { 
                     editBlock(e, data, editMenu);
                     delBlock(e, data, delMenu);
-                    // while (i != (data.blocks).length - 1) {
-                    //     readJSONBlocks(data.blocks[i].type, data, i)
-                    //     i++
-                    // }
                 }
             }, false);
 
@@ -208,14 +162,6 @@ function loadEditJSON(levelURI) {
                 return elem.className.split(' ').indexOf(className) > -1;
             }
 
-            containers.forEach((container) => {
-                data.blocks = []
-                i = 0
-                while (i < container.childNodes.length) {
-                    saveDataJSON(container, data, i)
-                    i++
-                }
-            })
             document.querySelector(".download").addEventListener('click', () => {
                 title = prompt("Entrer le nom du fichier:")
                 let jsonData = JSON.stringify(data)
@@ -225,4 +171,4 @@ function loadEditJSON(levelURI) {
 
 }
 
-loadEditJSON("./scripts/levels/level1.json")
+loadEditJSON("./scripts/levels/level1.jmpr")
